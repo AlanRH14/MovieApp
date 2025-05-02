644924b139868d2d265fd4cc980c3888ec603304
ALAN ANDRES ROSAS HERNANDEZ <alan.rosash@elektra.com.mx>
2025-05-02 11:51:11 -0600

    Config mappers

---
+++ app/src/main/java/com/example/movieapp/data/mapper_impl/MovieApiMapperImpl.kt
@@ -0,0 +1,43 @@
+package com.example.movieapp.data.mapper_impl
+
+import com.example.movieapp.common.movie.ApiMapper
+import com.example.movieapp.data.remote.models.MovieDto
+import com.example.movieapp.domain.models.Movie
+import com.example.movieapp.utils.GenreConstants
+
+class MovieApiMapperImpl : ApiMapper<List<Movie>, MovieDto> {
+
+    override fun mapToDomain(apiDto: MovieDto): List<Movie> {
+        return apiDto.results?.map { result ->
+            Movie(
+                backdropPath = formatEmptyValue(result?.backdropPath),
+                genreIds = formatGere(result?.genreIds),
+                id = result?.id ?: 0,
+                originalLanguage = formatEmptyValue(result?.originalLanguage, default = "language"),
+                originalTitle = formatEmptyValue(result?.originalTitle, default = "title"),
+                overview = formatEmptyValue(result?.overview, default = "overview"),
+                popularity = result?.popularity ?: 0.0,
+                posterPath = formatEmptyValue(result?.posterPath),
+                releaseDate = formatEmptyValue(result?.releaseDate, default = "date"),
+                title = formatEmptyValue(result?.title, default = "title"),
+                voteAverage = result?.voteAverage ?: 0.0,
+                voteCount = result?.voteCount ?: 0,
+                video = result?.video ?: false
+            )
+        } ?: emptyList()
+    }
+
+    private fun formatEmptyValue(value: String?, default: String = ""): String {
+        return if (value.isNullOrEmpty()) {
+            "Unknown $default"
+        } else {
+            value
+        }
+    }
+
+    private fun formatGere(genreIds: List<Int?>?): List<String> {
+        return genreIds?.map {
+            GenreConstants.getGenreNameById(it ?: 0)
+        } ?: emptyList()
+    }
+}
