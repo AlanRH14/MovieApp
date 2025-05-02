bbf15d43e2c8c3de44796fa95e62b8835381f129
ALAN ANDRES ROSAS HERNANDEZ <alan.rosash@elektra.com.mx>
2025-05-02 13:57:07 -0600

    Add Inject Constructor in HomeViewModel for error to build app

--- app/src/main/java/com/example/movieapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/home/HomeViewModel.kt
@@ -9,9 +9,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel(
+class HomeViewModel @Inject constructor(
     private val repository: MovieRepository
 ) : ViewModel() {
 
