891434955060e350bcbed5dad4e5abf114aa72e5
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-13 12:58:48 -0600

    Padding in BodyContent

--- app/src/main/java/com/example/movieapp/presentation/home/widgets/BodyContent.kt
+++ app/src/main/java/com/example/movieapp/presentation/home/widgets/BodyContent.kt
@@ -1,6 +1,8 @@
 package com.example.movieapp.presentation.home.widgets
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -12,6 +14,7 @@ import com.example.movieapp.R
 import com.example.movieapp.domain.models.movie.Movie
 import com.example.movieapp.presentation.components.HeaderMovieList
 import com.example.movieapp.presentation.home.components.MovieCoverImage
+import com.example.movieapp.ui.theme.ItemSpacing
 
 @Composable
 fun BodyContent(
@@ -23,31 +26,33 @@ fun BodyContent(
     LazyColumn(modifier = modifier) {
         item {
             Card(modifier = Modifier.fillMaxWidth()) {
-                HeaderMovieList(
-                    title = stringResource(R.string.discover_movies),
-                    contentDescription = stringResource(R.string.more_discover_movies_button)
-                )
+                Column(modifier = Modifier.padding(horizontal = ItemSpacing)) {
+                    HeaderMovieList(
+                        title = stringResource(R.string.discover_movies),
+                        contentDescription = stringResource(R.string.more_discover_movies_button)
+                    )
 
-                LazyRow {
-                    items(discoverMovies) { discoverMovie ->
-                        MovieCoverImage(
-                            movie = discoverMovie,
-                            onMovieClick = onMovieClick
-                        )
+                    LazyRow {
+                        items(discoverMovies) { discoverMovie ->
+                            MovieCoverImage(
+                                movie = discoverMovie,
+                                onMovieClick = onMovieClick
+                            )
+                        }
                     }
-                }
 
-                HeaderMovieList(
-                    title = stringResource(R.string.trending_now),
-                    contentDescription = stringResource(R.string.trending_now_button)
-                )
+                    HeaderMovieList(
+                        title = stringResource(R.string.trending_now),
+                        contentDescription = stringResource(R.string.trending_now_button)
+                    )
 
-                LazyRow {
-                    items(trendingMovies) { trendingMovie ->
-                        MovieCoverImage(
-                            movie = trendingMovie,
-                            onMovieClick = onMovieClick
-                        )
+                    LazyRow {
+                        items(trendingMovies) { trendingMovie ->
+                            MovieCoverImage(
+                                movie = trendingMovie,
+                                onMovieClick = onMovieClick
+                            )
+                        }
                     }
                 }
             }
