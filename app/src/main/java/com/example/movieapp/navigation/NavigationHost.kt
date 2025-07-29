fb50b0345691a6a3c9b897bc9a615b388d4aa097
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-29 13:08:20 -0600

    Config Movie Detail MVI project part. 15.

--- app/src/main/java/com/example/movieapp/navigation/NavigationHost.kt
+++ app/src/main/java/com/example/movieapp/navigation/NavigationHost.kt
@@ -3,7 +3,6 @@ package com.example.movieapp.navigation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
