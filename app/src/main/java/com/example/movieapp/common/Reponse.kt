16c2ba4770c5e29c2d13f72c044c9a94750c53c9
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-29 16:14:53 -0600

    Clean code part. 13.

--- app/src/main/java/com/example/movieapp/common/Reponse.kt
+++ app/src/main/java/com/example/movieapp/common/Reponse.kt
@@ -1,7 +1,7 @@
 package com.example.movieapp.common
 
-sealed class Response<out T>{
-    class Success<T>(val data:T): Response<T>()
-    class Error<T>(val error:Throwable?,val data:T? = null): Response<T>()
-    data object Loading: Response<Nothing>()
+sealed interface Response<out T>{
+    class Success<T>(val data:T): Response<T>
+    class Error<T>(val error:Throwable?,val data:T? = null): Response<T>
+    data object Loading: Response<Nothing>
 }
