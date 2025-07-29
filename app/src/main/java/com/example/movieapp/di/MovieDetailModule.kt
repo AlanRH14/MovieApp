27d8c65ba3c8d22ceda0ff73a2e62eb0d14470ff
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-29 07:36:16 -0600

    Config project Koin in project part. 13.

--- app/src/main/java/com/example/movieapp/di/MovieDetailModule.kt
+++ app/src/main/java/com/example/movieapp/di/MovieDetailModule.kt
@@ -2,6 +2,7 @@ package com.example.movieapp.di
 
 import com.example.movieapp.data.remote.repository.movie_detail.MovieDetailRepositoryImpl
 import com.example.movieapp.domain.repository.movie_detail.MovieDetailRepository
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val movieDetailModule = module {
@@ -9,8 +10,8 @@ val movieDetailModule = module {
     single<MovieDetailRepository> {
         MovieDetailRepositoryImpl(
             movieDetailApiService = get(),
-            apiDetailMapper = get(),
-            apiMovieMapper = get(),
+            apiDetailMapper = get(named("MovieDetailApiMapper")),
+            apiMovieMapper = get(named("MovieApiMapper")),
         )
     }
 }
--- app/src/main/java/com/example/movieapp/di/MovieModule.kt
+++ app/src/main/java/com/example/movieapp/di/MovieModule.kt
@@ -2,13 +2,14 @@ package com.example.movieapp.di
 
 import com.example.movieapp.data.remote.repository.movie.MovieRepositoryImpl
 import com.example.movieapp.domain.repository.movie.MovieRepository
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val movieModule = module {
     single<MovieRepository> {
         MovieRepositoryImpl(
             movieApiService = get(),
-            apiMapper = get()
+            apiMapper = get(named("MovieApiMapper"))
         )
     }
 }
