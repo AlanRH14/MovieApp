7f5f71c2791ed1fe271fc54506564a1b65edcebd
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-29 08:05:21 -0600

    Config Home MVI project part. 11.

--- app/src/main/java/com/example/movieapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/movieapp/presentation/home/HomeScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.movieapp.presentation.home.widgets.TopContent
 import com.example.movieapp.presentation.home.widgets.BodyContent
 import com.example.movieapp.presentation.home.widgets.shimmer.HomeLoadingScreen
