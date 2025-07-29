06527b0cdae83f0976d3551c9ec52838287268df
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-29 07:48:12 -0600

    Config Home MVI project part. 5.

--- app/src/main/java/com/example/movieapp/presentation/home/HomeUIEvent.kt
+++ app/src/main/java/com/example/movieapp/presentation/home/HomeUIEvent.kt
@@ -3,4 +3,5 @@ package com.example.movieapp.presentation.home
 sealed interface HomeUIEvent {
     data class OnMovieClicked(val movieID: String): HomeUIEvent
     data object OnFetchDiscoverMovie: HomeUIEvent
+    data object OnFetchTrendingMovie: HomeUIEvent
 }
