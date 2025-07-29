5a7a8fd1b58c11bd02d52090ec97db112913feee
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-29 13:10:07 -0600

    Config Movie Detail MVI project part. 18.

--- app/src/main/java/com/example/movieapp/navigation/Route.kt
+++ app/src/main/java/com/example/movieapp/navigation/Route.kt
@@ -1,16 +1,6 @@
 package com.example.movieapp.navigation
 
-import com.example.movieapp.utils.NavigationKeys.FILM_SCREEN
-
 sealed interface Route {
-    data object HomeScreen: Route
-
-    data class FilmScreen(
-        val route: String = FILM_SCREEN,
-        val routeWithArgs: String = "$route/{id}"
-    ): Route {
-        fun getRouteWithArgs(id: Int): String {
-            return "$route/$id"
-        }
-    }
+    data object HomeScreen : Route
+    data class FilmScreen(val movieID: Int) : Route
 }
